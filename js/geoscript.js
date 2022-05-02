60b6b15e847efdf8637d3221a54cbbdc670b6b14
Ilia Zubov <97738404+ThePugSummoner@users.noreply.github.com>
2022-05-03 00:04:25 +0300

    pages, images, css and js files added

---
+++ js/geoscript.js
@@ -0,0 +1,251 @@
+let questions = [
+
+    {
+        text: "Mariaanien hauta on läntisellä Tyynellämerellä sijaitseva maailman syvin tunnettu syvänmeren hauta. Sen pituus on noin 2 550 kilometriä ja keskimääräinen leveys 69 kilometriä. Mariaanien hauta on muodostunut Tyynenmeren laatan subduktoituessa Filippiinien laatan alle. Haudan pohjalla vedessä vallitsee 1 086 baarin paine, joka on tuhatkertainen normaaliin ilmanpaineeseen verrattuna. Mikä on sen syvin kohta?",
+        imgFile: "../images/diver-108881_640.jpg",
+        answer: [
+            "n.11000m",
+            "n.9000m",
+            "n.8000m",
+            "n.10000m"]
+    },
+
+    {
+        text: "Tämä mausoleumi sijaitsee Agrassa, Intiassa. Mausoleumi rakennettiin vuosien 1632 ja 1648 välillä. Rakennuskompleksin muut rakennukset kuten moskeija ja portti valmistuivat vuoteen 1653 mennessä. Valkoisesta marmorista rakennetun mausoleumin lisäksi rakennuskompleksiin kuuluu reunustava muuri, puisto, portti, moskeija ja vierastalo. Mausoleumi on koristeltu arvokkailla kivireliefeillä ja -upotuksilla sekä kalligrafialla. Mikä sen nimi on?",
+        imgFile: "../images/cultural-tourism-5264542_640.jpg",
+        answer: [
+            "Shah Jahan",
+            "Mumtaz Mahal",
+            "Taj Mahal",
+            "Humayun"]
+    },
+
+    {
+        text: "Kheopsin pyramidi (myös Khufun pyramidi tai Suuri pyramidi) on suurin ja tunnetuin egyptiläisistä pyramideista. Neljännen dynastian faarao Kheops (Khufu) oletettavasti rakennutti pyramidin hautapaikakseen noin 2560 eaa. Se oli maailman korkein rakennus yli 3 800 vuoden ajan. Missä kaupungissa se on?",
+        imgFile: "../images/egypt-2133951_640.jpg",
+        answer: [
+            "Kairossa",
+            "Luxorisaa",
+            "Aleksandriassa",
+            "Gizassa"]
+    },
+
+    {
+        text: "Eiffelin torni on Ranskan pääkaupungissa Pariisissa sijaitseva torni. Rakentajansa Gustave Eiffelin mukaan nimetty torni on Pariisin kaupungin symboli, Euroopan kuuluisimpia nähtävyyksiä ja koko Ranskan suosituin matkailukohde. Eiffel-torni on televisioantenni. Minkä korkuinen se on?",
+        imgFile: "../images/eiffel-tower-3349075_640.jpg",
+        answer: [
+            "390m",
+            "324m",
+            "420m",
+            "286m"]
+    },
+
+    {
+        text: "Petronas Twin Towers ovat vuonna 1999 valmistuneet 452 metrin korkuiset pilvenpiirtäjät. Ne olivat maailman korkeimmat rakennukset vuodesta 1999 vuoteen 2004 asti, jolloin Taipei 101 valmistui. Ne ovat yhä maailman korkeimmat kaksoistornit ja korkeimmat 1900-luvulla valmistuneet rakennukset. Missä maassa ne ovat?",
+        imgFile: "../images/malaysia-2289403_640.jpg",
+        answer: [
+            "Malesiassa",
+            "Indonesiassa",
+            "Thaimaassa",
+            "Vietnamissä"]
+    },
+
+    {
+        text: "Chichén Itzá on kuuluisin ja parhaiten kunnostettu esikolumbiaaninen arkeologinen alue ja historiallinen kaupunki Jukatanin niemimaalla. Sen rakentamisen aloittivat mayat. Chichén Itzá kuuluu Unescon maailmanperintöluetteloon. Tämä pyramidi tunnettu nimellä Kukulcanin temppeli. Missä päin maailma se sijaitsee?",
+        imgFile: "../images/mexico-3774303_640.jpg",
+        answer: [
+            "Egyptissä",
+            "Meksikossa",
+            "Kiinassa",
+            "Jordaniassa"]
+    },
+
+    {
+        text: "Parthenon on antiikkinen temppeli. Parthenonin rakennustyö alkoi vuonna 447 eaa., ja rakennus oli pääosin valmis vuonna 438 eaa., jolloin järjestettiin sen juhlalliset vihkiäiset. Koristelutyöt jatkuivat kuitenkin ainakin vuoteen 432 eaa. asti. Temppeli oli antiikin ensimmäinen ja suurin kokonaan marmorista rakennettu temppeli. Missä maassa se on? ",
+        imgFile: "../images/greece-1594689_640.jpg",
+        answer: [
+            "Kyproksessa",
+            "Turkissa",
+            "Italiassa",
+            "Kreikassa"]
+    },
+
+    {
+        text: "Kellotorni on 96,3 metriä korkea ja sijaitsee Westminsterissä Thamesjoen varrella. Se on muodostunut Lontoon ja koko Yhdistyneen kuningaskunnan symboliksi. Kellotornin suunnittelivat Charles Barry ja Augustus Pugin. Se valmistui vuonna 1856 rakennustöiden kestettyä 13 vuotta. Vuodesta 2012 alkaen sen virallinen nimi on ... torni.",
+        imgFile: "../images/big-ben-1143631_640.jpg",
+        answer: [
+            "BigBen",
+            "Elisabetin",
+            "Pyhän Tapanin",
+            "Kello"]
+    },
+
+    {
+        text: "Colosseum oli valtakunnan suurin amfiteatteri, ja sinne mahtui ilmeisesti yli 50 000 katsojaa. Colosseumilla järjestettiin sen ensimmäisten vuosisatojen aikana gladiaattoritaisteluita, metsästysnäytöksiä ja muita näytöksiä sekä teloituksia. Tämä on tunnetuin nähtävyys kohde Italiassa. Missä kaupungissa se on?",
+        imgFile: "../images/colosseum-3456772_640.jpg",
+        answer: [
+            "Venetsiassa",
+            "Napolissa",
+            "Roomassa",
+            "Milanossa"]
+    },
+
+    {
+        text: "Kalteva torni on italialaisen kaupungin tuomiokirkon kellotorni Piazza dei Miracolin alueella. Torni on tullut kuuluisaksi siitä, että se on kallellaan. Romaanista tyyliä edustavan tornin rakentaminen aloitettiin vuonna 1173. Sen kallistuminen aiheutti taukoja rakentamiseen, ja kellotapuli valmistui vasta vuonna 1370. Tornissa on seitsemän kirkonkelloa. Tornin kallistuminen saatiin pysäytettyä 1990-luvulla aloitetussa suoristushankkeessa. Missä kaupungissa kalteva torni sijaitsee?",
+        imgFile: "../images/italy-4681065_640.jpg",
+        answer: [
+            "Pisassa",
+            "Roomassa",
+            "Palermossa",
+            "Bolognassa"]
+    },
+//10 questions
+    {
+        text: "Japanin Alpit ovat vuoristo Japanin pääsaaren Honshūn keskiosassa. Japanin korkein vuoristo koostuu Hida-, Kiso- ja Akaishi-vuoristoista (Pohjois-, Keski- ja Etelä-Alpeista).Mikä on Japanin korkein vuori?",
+        imgFile: "../images/mount-fuji-6748510_640.jpg",
+        answer: [
+            "Fuji",
+            "Kitadake",
+            "Hotakadake",
+            "Komagatake"]
+    },
+
+    {
+        text: "Mikä on maailman korkein vuori?",
+        imgFile: "../images/mount-everest-5295184_640.jpg",
+        answer: [
+            "Everest",
+            "Mont Blanc",
+            "K2",
+            "Aconcagua"]
+    },
+
+    {
+        text: "Tämä valtio sijaitsee Etelä-Amerikan eteläosassa Atlantin länsirannikolla. Se ulottuu trooppisesta pohjoisesta Etelä-Amerikan antarktiseen eteläkärkeen. Maan eläinpopulaatioon kuuluu niin apinoita, jaguaareja kuin pingviinejäkin.",
+        imgFile: "../images/international-2690834_640.jpg",
+        answer: [
+            "Bolivia",
+            "Argentina",
+            "Peru",
+            "Chile"]
+    },
+
+    {
+        text: "Brasilia on Etelä-Amerikan suurin valtio sekä pinta-alaltaan että asukasluvultaan ja se on myös sekä pinta-alaltaan että asukasluvultaan maailman viidenneksi suurin valtio. Virallinen kieli on portugali. Mikä on Brasilian pääkaupunki?",
+        imgFile: "../images/flag-742573_640.jpg",
+        answer: [
+            "São Paulo",
+            "Rio de Janeiro",
+            "Brasilia",
+            "Blumenau"]
+    },
+
+    {
+        text: "Mikä on maailman suurin valtameri?",
+        imgFile: "../images/cruise-1578528_640.jpg",
+        answer: [
+            "Tyyni",
+            "Atlannin",
+            "Intian",
+            "Pohjoinen"]
+    },
+
+    {
+        text: "Petra perustettiin nabatealaisten pääkaupungiksi kuudennella vuosisadalla eaa.[2] Se tunnetaan erityisesti kiveen kaiverretusta arkkitehtuuristaan ja vesijohtojärjestelmästään. Suurimmillaan Petrassa oli mahdollisesti yli 20 000 asukasta, ja se oli merkittävä kauppakeskus. Kaupunki hylättiin tiettävästi 600-luvun lopulla maanjäristysten ja kaupan vähenemisen seurauksena, minkä jälkeen se vaipui unohduksiin. Nykyisin Petra on suosittu vierailukohde, ja se on ollut Unescon maailmanperintöluettelossa vuodesta 1985. Missä maassa Petra sijaitsee?",
+        imgFile: "../images/petra-6294051_640.jpg",
+        answer: [
+            "Egyptissä",
+            "Turkissa",
+            "Jordaniassa",
+            "Syyriassa"]
+    },
+    //16 questions
+    {
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ipsum aliquid eius reprehenderit magnam neque iure. Quae ab aut perferendis cupiditate obcaecati voluptate voluptates reprehenderit fugiat, repellat dolor sequi dolorum? Quae culpa ipsa neque quaerat veniam laboriosam corrupti deserunt harum corporis molestias ratione accusamus odit assumenda sint recusandae porro eligendi repellat amet doloribus magnam cumque, animi sunt expedita? Eveniet, dignissimos.",
+        imgFile: "../images/petra-6294051_640.jpg",
+        answer: [
+            "61",
+            "62",
+            "63",
+            "64"]
+    },
+
+    {
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ipsum aliquid eius reprehenderit magnam neque iure. Quae ab aut perferendis cupiditate obcaecati voluptate voluptates reprehenderit fugiat, repellat dolor sequi dolorum? Quae culpa ipsa neque quaerat veniam laboriosam corrupti deserunt harum corporis molestias ratione accusamus odit assumenda sint recusandae porro eligendi repellat amet doloribus magnam cumque, animi sunt expedita? Eveniet, dignissimos.",
+        imgFile: "../images/italy-4681065_640.jpg",
+        answer: [
+            "65",
+            "66",
+            "67",
+            "68"]
+    },
+
+    {
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ipsum aliquid eius reprehenderit magnam neque iure. Quae ab aut perferendis cupiditate obcaecati voluptate voluptates reprehenderit fugiat, repellat dolor sequi dolorum? Quae culpa ipsa neque quaerat veniam laboriosam corrupti deserunt harum corporis molestias ratione accusamus odit assumenda sint recusandae porro eligendi repellat amet doloribus magnam cumque, animi sunt expedita? Eveniet, dignissimos.",
+        imgFile: "../images/italy-4681065_640.jpg",
+        answer: [
+            "69",
+            "70",
+            "71",
+            "72"]
+    },
+
+    {
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ipsum aliquid eius reprehenderit magnam neque iure. Quae ab aut perferendis cupiditate obcaecati voluptate voluptates reprehenderit fugiat, repellat dolor sequi dolorum? Quae culpa ipsa neque quaerat veniam laboriosam corrupti deserunt harum corporis molestias ratione accusamus odit assumenda sint recusandae porro eligendi repellat amet doloribus magnam cumque, animi sunt expedita? Eveniet, dignissimos.",
+        imgFile: "../images/italy-4681065_640.jpg",
+        answer: [
+            "73",
+            "74",
+            "75",
+            "76"]
+    },
+    //20 questions
+];
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+let num = 0;
+
+let forward = document.getElementById("btn6");
+
+forward.addEventListener("click", nextQuestion);
+
+function nextQuestion() {
+
+    num++;
+
+    let rndNumber = getRndInteger(0, questions.length - 1);
+    
+    for (let i = 0; i < questions.length; i++) {
+        document.getElementById("questionHere").innerHTML = questions[rndNumber].text;
+        document.getElementById("picture").innerHTML = "<img src ='"+ questions[rndNumber].imgFile +"' class='rounded mx-auto d-block my-5' alt=''/>";
+        document.getElementById("btn1").innerHTML = questions[rndNumber].answer[0];
+        document.getElementById("btn2").innerHTML = questions[rndNumber].answer[1];
+        document.getElementById("btn3").innerHTML = questions[rndNumber].answer[2];
+        document.getElementById("btn4").innerHTML = questions[rndNumber].answer[3];
+        document.getElementById("num").innerHTML = num;
+    }
+    questions.splice(rndNumber, 1);
+
+    if(num > 4) {
+        forward.disabled = true;
+        forward.innerHTML = "Lähetä";
+        forward.addEventListener("click", sendAnswers);
+    }
+}
+
+let answer1 = document.getElementById("btn1");
+let answer2 = document.getElementById("btn2");
+let answer3 = document.getElementById("btn3");
+let answer4 = document.getElementById("btn4");
+
+answer1.addEventListener("click", lockAnswer);
+answer2.addEventListener("click", lockAnswer);
+answer3.addEventListener("click", lockAnswer);
+answer4.addEventListener("click", lockAnswer);
+
+function lockAnswer () {
+
+}
